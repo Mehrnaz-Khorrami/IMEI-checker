bbfc9649d91e364ad879bd2b6e5c9408a49a51cc
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-01-17 12:41:48 +0330

    feat: set custom background color for project

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,6 +8,17 @@ import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  theme: {
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme: {
+        dark: false, // Set to true if you want a dark theme
+        colors: {
+          background: '#FFFAEC',
+        },
+      },
+    },
+  },
   styles: {
     configFile: "src/styles/settings.scss",
   },

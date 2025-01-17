32fb7f313643037fd946eb0e6ef72ec84dd58533
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-01-17 15:34:59 +0330

    build: custom text field for use everywhere

---
+++ src/components/shared/textField.vue
@@ -0,0 +1,12 @@
+<template>
+  <customTextField
+    variant="outlined"
+    bg-color="white"
+    density="compact"
+  />
+</template>
+<script setup>
+import { VTextField as customTextField } from "vuetify/components";
+</script>
+
+<style scoped lang="css"></style>

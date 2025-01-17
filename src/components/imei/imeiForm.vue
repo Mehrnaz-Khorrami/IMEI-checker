b3fa0e88e99bb9fc4cc153c5eb97ccdbb997765d
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-01-17 15:35:59 +0330

    feat: imei checker form

---
+++ src/components/imei/imeiForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="12">
+      <SharedTextField label="شماره جعبه" />
+    </v-col>
+    <v-col cols="12">
+      <SharedTextField label="سریال نامبر۱" />
+    </v-col>
+    <v-col cols="12">
+      <SharedTextField label="سریال نامبر۲" />
+    </v-col>
+    <v-col cols="6">
+      <input
+        id="fileInput"
+        ref="fileInput"
+        type="file"
+        accept=".csv, .txt"
+        class="hi"
+        style="display: none"
+      />
+      <v-btn
+        color="#578E7E"
+        variant="outlined"
+        width="100%"
+        class="ml-md-1"
+      >
+        <label
+          id="fileInputLabel"
+          for="fileInput"
+        >
+          انتخاب فایل
+        </label>
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+<script setup>
+import SharedTextField from "@/components/shared/textField.vue";
+import { ref } from "vue";
+
+const fileInput = ref(null);
+</script>
+
+<style scoped lang="css">
+.hi {
+  width: 100%;
+}
+</style>

510b11b11b0ec1ce314ad6194425fb8ebd865134
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-01-17 16:21:48 +0330

    build: layout component for future layouts

---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <!--    <v-app-bar app scroll-behavior="elevate" color="#F1F0E8">-->
+    <!--      &lt;!&ndash; Your app bar content here &ndash;&gt;-->
+    <!--      <v-toolbar-title>My Application</v-toolbar-title>-->
+    <!--    </v-app-bar>-->
+    <v-main>
+      <!-- The slot will display the content of the pages -->
+      <slot></slot>
+    </v-main>
+  </v-app>
+</template>
+<script setup></script>
+
+<style scoped lang="css"></style>

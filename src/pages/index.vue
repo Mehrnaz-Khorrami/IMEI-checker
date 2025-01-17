8943c97781ecb764191ff76dd72854eb09f51153
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-01-17 15:35:25 +0330

    fix: change the columns number

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <v-row no-gutters>
-    <v-col cols="5">
+    <v-col cols="4">
+      <ImeiChecker />
     </v-col>
-    <v-col cols="7">
+    <v-col cols="8">
       <ImeiTable />
     </v-col>
   </v-row>
 </template>
 
 <script setup>
-import ImeiTable from "@/components/imeiTable.vue";
+import ImeiTable from "@/components/imei/imeiTable.vue";
+import ImeiChecker from "@/components/imei/imeiChecker.vue";
 </script>
